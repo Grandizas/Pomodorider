bfdc93fccd99741b2b3a60789317ee3790d24e19
Grandizas <grazvydaszilius2001@gmail.com>
2026-01-31 15:25:55 +0200

    chore: enable git lfs for large assets

--- app/composable/applyTheme.ts
+++
@@ -1,9 +0,0 @@
-import { type ThemeKey, themes } from '~~/themes/themes';
-
-export function applyTheme(themeKey: ThemeKey) {
-    const theme = themes[themeKey];
-
-    Object.entries(theme.colors).forEach(([key, value]) => {
-        document.documentElement.style.setProperty(`--color-${key}`, value);
-    });
-}
